aa505d89186f6cec226d1aa7c5d9ca1380d7a987
CricketData <119282951+CricketData@users.noreply.github.com>
2022-11-30 10:38:31 +0530

    Update Fantasy_squad.py

--- Fantasy_squad.py
+++ Fantasy_squad.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 apikey = "%APIKEY%"
-id = "1f16f05e-e25c-43a1-ba00-331b1d2c3b8a"
-response = requests.get("https://api.cricapi.com/v1/cricScore?apikey="+apikey+"&id="+id)
+match_id = "1f16f05e-e25c-43a1-ba00-331b1d2c3b8a"
+response = requests.get("https://api.cricapi.com/v1/match_squad?apikey=apikey&id=match_id)
 def myobj(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     data = json.loads(text)['data']
